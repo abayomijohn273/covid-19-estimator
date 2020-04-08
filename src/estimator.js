eff455f8746123b28f4372c9cc149732f6cb661e
Abayomi Olatunji <https://abayomijohn273@bitbucket.org>
2020-04-08 17:58:01 +0100

    Challenge 2 modified

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,10 @@
 // const covid19ImpactEstimator = (data) => data;
 
 const covid19ImpactEstimator = (data) => {
-  const input = data;
 
   //   step 1 for impact computation
   const impact = {
-    currentlyInfected: input.reportedCases * 10
+    currentlyInfected: data.reportedCases * 10
   };
   // step 2  for impact computation
   impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
@@ -14,11 +13,14 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * (15 / 100);
 
   // step 4 for impact computation
-  impact.hospitalBedsByRequestedTime = input.totalHospitalBeds * impact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * impact.severeCasesByRequestedTime;
+
+  // step 5 for impact computation
+//   impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * impact.severeCasesByRequestedTime;
 
   // step 1 for severeImpact computation
   const severeImpact = {
-    currentlyInfected: input.reportedCases * 50
+    currentlyInfected: data.reportedCases * 50
   };
   // step 2  for severeImpact computation
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
@@ -27,7 +29,7 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * (15 / 100);
 
   // step 4 for severeImpact computation
-  severeImpact.hospitalBedsByRequestedTime = input.totalHospitalBeds * severeImpact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = data.totalHospitalBeds * severeImpact.severeCasesByRequestedTime;
 
   return { data, impact, severeImpact };
 };
