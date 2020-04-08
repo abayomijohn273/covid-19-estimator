a4db8f07cd363503a29910bb427f53e7464b6de6
Abayomi Olatunji <https://abayomijohn273@bitbucket.org>
2020-04-08 17:31:56 +0100

    Challenge 2 completed

--- src/estimator.js
+++ src/estimator.js
@@ -10,12 +10,28 @@ const covid19ImpactEstimator = data => {
   // step 2  for impact computation
   impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
 
+  //   step 3 for impact computation
+  impact.severeCasesByRequestedTime =
+    impact.infectionsByRequestedTime * (15 / 100);
+
+  // step 4 for impact computation
+  impact.hospitalBedsByRequestedTime =
+    input.totalHospitalBeds * impact.severeCasesByRequestedTime;
+
   // step 1 for severeImpact computation
   const severeImpact = {
     currentlyInfected: input.reportedCases * 50
   };
   // step 2  for severeImpact computation
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+
+  //   step 3 for severeImpact computation
+  severeImpact.severeCasesByRequestedTime =
+    severeImpact.infectionsByRequestedTime * (15 / 100);
+
+  // step 4 for severeImpact computation
+  severeImpact.hospitalBedsByRequestedTime =
+    input.totalHospitalBeds * severeImpact.severeCasesByRequestedTime;
 
   return { data, impact, severeImpact };
 };
